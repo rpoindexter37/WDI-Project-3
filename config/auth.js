bb245ee5449b047f49564529318a4040c6823bc3
Ryan Poindexter <rpoindexter37@gmail.com>
2017-01-20 14:45:59 -0800

    auth fix

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   'facebookAuth': {
     'clientID': process.env.facebookID,
     'clientSecret': process.env.facebookSecret,
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+    'callbackURL': process.env.facebookCallbackURL,
     'profileFields': ['emails', 'displayName']
   }
 }
