4d28e6a6b16411ac1c069e636aef6d3fbf4c6938
Ryan Poindexter <rpoindexter37@gmail.com>
2017-01-20 14:18:15 -0800

    sent auth.js keys to .env file

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebookAuth': {
-    'clientID': '1725933144401933',
-    'clientSecret': '422aceb9ff0fc6012ae171a7de98c99f',
+    'clientID': process.env.facebookID,
+    'clientSecret': process.env.facebookSecret,
     'callbackURL': 'http://localhost:3000/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
